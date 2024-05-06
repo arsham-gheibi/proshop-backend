02c9d4a9b697a72fc06c73a4e6b4f0e5c75e1d14
arsham-gheibi <gheibi.arsham@gmail.com>
2024-05-06 17:14:00 +0330

    feat: add static routing to app/urls.py

--- app/app/urls.py
+++ app/app/urls.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 
-urlpatterns = (
-    path('admin/', admin.site.urls),
-    path('', include('rest_framework.urls'))
-)
+urlpatterns = [
+    path('drf/', include('rest_framework.urls')),
+    path('', include('store.urls'))
+]
+
+if settings.DEBUG:
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+    urlpatterns.append(path('admin/', admin.site.urls))
+    urlpatterns.append(path('__debug__/', include('debug_toolbar.urls')))
