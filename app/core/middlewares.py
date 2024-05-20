ae85ef9414c493fc0639a92fb3bf66acc94ba1cc
arsham-gheibi <gheibi.arsham@gmail.com>
2024-05-20 13:51:34 +0330

    feat: update AbstractGraphQLView

--- app/core/middlewares.py
+++ app/core/middlewares.py
@@ -6,10 +6,15 @@ from graphene_django.views import GraphQLView
 class AbstractGraphQLView(GraphQLView):
     @csrf_exempt
     def dispatch(self, request, *args, **kwargs):
-        body_unicode = request.body.decode('utf-8')
-        query = json.loads(body_unicode).get('query', '')
+        try:
+            body_unicode = request.body.decode('utf-8')
+            query = json.loads(body_unicode).get('query', '')
 
-        if 'mutation' in query:
-            return csrf_protect(super().dispatch)(request, *args, **kwargs)
+            if 'mutation' in query.lower():
+                return csrf_protect(super().dispatch)(request, *args, **kwargs)
+
+        except (json.JSONDecodeError, UnicodeDecodeError):
+            # Handle decoding errors and log them if necessary
+            pass
 
         return super().dispatch(request, *args, **kwargs)
