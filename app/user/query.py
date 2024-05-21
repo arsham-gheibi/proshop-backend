8ec1c6b02e1ee38f1b9aff62d308596121b802d2
arsham-gheibi <gheibi.arsham@gmail.com>
2024-05-21 18:48:15 +0330

    feat: add user Query

--- app/user/query.py
+++ app/user/query.py
@@ -9,11 +9,20 @@ User = get_user_model()
 
 class Query:
     all_users = graphene.List(UserType)
+    user = graphene.Field(
+        UserType,
+        id=graphene.UUID(required=True)
+    )
+
     profile = graphene.Field(UserType)
 
     @staff_member_required
     def resolve_all_users(root, info):
         return User.objects.all()
+
+    @staff_member_required
+    def resolve_user(root, info, id):
+        return User.objects.get(id=id)
 
     @login_required
     def resolve_profile(root, info):
