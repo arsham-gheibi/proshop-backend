ea743d6171c682f0018f4e4e595f996913c91ee8
arsham-gheibi <gheibi.arsham@gmail.com>
2024-05-21 17:53:38 +0330

    feat: update CORS_ALLOW_METHODS

--- app/app/settings.py
+++ app/app/settings.py
@@ -30,7 +30,7 @@ CORS_ALLOWED_ORIGINS.extend(
     )
 )
 
-CORS_ALLOW_METHODS = ['POST']
+CORS_ALLOW_METHODS = ('GET', 'POST')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -134,7 +134,6 @@ if DEBUG:
     INTERNAL_IPS = [ip[:-1] + '1' for ip in ips] + \
         ['127.0.0.1', '10.0.2.2'] + ALLOWED_HOSTS
 
-    CORS_ALLOW_METHODS.append('GET')
     INSTALLED_APPS.append('debug_toolbar')
     MIDDLEWARE.append('debug_toolbar.middleware.DebugToolbarMiddleware')
 
